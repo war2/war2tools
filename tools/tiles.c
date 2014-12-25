5e20679bdddc707b315f8886df714d21c44cc249
Jean Guyomarc'h <jean.guyomarch@gmail.com>
2014-12-25 23:10:20 +0100

    tiles.c: read whole image and pre-generate switch

--- tools/tiles.c
+++ tools/tiles.c
@@ -1,9 +1,16 @@
 #include "ppm.h"
 #include "../pudviewer.h"
 
+typedef struct {
+   uint16_t tile;
+   Col      col;
+} Mem;
+
+
 static inline int
 _str2int(const char *str)
 {
+   if (!str) return -1;
    return strtol(str, NULL, 10);
 }
 
@@ -13,13 +20,16 @@ main(int    argc,
 {
    Pud *pud;
    Col *ppm;
-   int w, h, i;
+   int w, h, i, j;
    uint16_t word;
    int x, y;
+   Mem mem[1024];
+   int count = 0, k = 0, idx;
+   char add;
 
-   if (argc != 5)
+   if ((argc < 3) || (argc == 4))
      {
-        fprintf(stderr, "*** Usage: <file.pud> <file.ppm> <X> <Y>\n");
+        fprintf(stderr, "*** Usage: <file.pud> <file.ppm> [X, Y]\n");
         return 1;
      }
 
@@ -42,21 +52,77 @@ main(int    argc,
      }
 
    /* Get tile */
-   x = _str2int(argv[3]);
-   y = _str2int(argv[4]);
-   word = pud_tile_at(pud, x, y);
-   if (word == 0)
+   if (argc == 5)
      {
-        fprintf(stderr, "*** pud_tile_at() failed\n");
-        pud_free(pud);
-        free(ppm);
-        return 3;
+        x = _str2int(argv[3]);
+        y = _str2int(argv[4]);
+     }
+   else
+     {
+        x = -1;
+        y = -1;
      }
 
-   i = y * w + x;
-   printf("Tile [0x%04x] => { %02x, %02x, %02x }\n",
-          ppm[i].r, ppm[i].g, ppm[i].b);
+   if ((x < 0) || (y < 0))
+     {
+        /* For all tiles */
+        for (j = 0; j < h; j++)
+          {
+             for (i = 0; i < w; i++)
+               {
+                  word = pud_tile_at(pud, i, j);
+                  if (word == 0)
+                    {
+                       fprintf(stderr, "*** pud_tile_at(%i, %i) failed\n", i, j);
+                       pud_free(pud);
+                       free(ppm);
+                       return 3;
+                    }
+                  idx = (j * w) + i;
 
+                  add = 1;
+                  for (k = 0; k < count; k++)
+                    {
+                       if (mem[k].tile == word)
+                         {
+                            add = 0;
+                            break;
+                         }
+                    }
+                  if (add)
+                    {
+                       mem[count].tile = word;
+                       mem[count].col = ppm[idx];
+                       count++;
+                    }
+               }
+          }
+        printf("switch (tile)\n"
+               "  {\n");
+        for (k = 0; k < count; k++)
+          {
+             printf("   case 0x%04x: return _color_make(0x%02x, 0x%02x, 0x%02x);\n",
+                    mem[k].tile, mem[k].col.r, mem[k].col.g, mem[k].col.b);
+          }
+        printf("   default: break;\n");
+        printf("  }\n");
+     }
+   else
+     {
+        /* Do job for tile at X Y */
+        word = pud_tile_at(pud, x, y);
+        if (word == 0)
+          {
+             fprintf(stderr, "*** pud_tile_at() failed\n");
+             pud_free(pud);
+             free(ppm);
+             return 3;
+          }
+
+        i = y * w + x;
+        printf("0x%04x 0x%02x 0x%02x 0x%02x\n",
+               word, ppm[i].r, ppm[i].g, ppm[i].b);
+     }
 
    pud_free(pud);
    free(ppm);

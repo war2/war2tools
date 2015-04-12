f82a5950557ea226d70435df7e00412ad2492658
Jean Guyomarc'h <jean.guyomarch@gmail.com>
2015-04-12 14:10:46 +0200

    Split repository. Now editor is war2edit while what remains here is war2tools

--- bin/menu.h
+++
@@ -1,9 +0,0 @@
-#ifndef _MENU_H_
-#define _MENU_H_
-
-Eina_Bool menu_add(Editor *ed);
-void menu_enabled_set(Editor *ed, Eina_Bool set);
-void menu_unit_selection_reset(Editor *ed);
-
-#endif /* ! _MENU_H_ */
-

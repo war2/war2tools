a61754b2887c17d8266c024a9a452a69cb99b531
Jean Guyomarc'h <jean.guyomarch@gmail.com>
2015-12-30 15:13:20 +0100

    tools/export_tiles: more flexible export of tiles

--- tools/extract_tiles.c
+++ tools/extract_tiles.c
@@ -7,37 +7,46 @@
 
 #include <war2.h>
 #include "../include/debug.h"
+#include <Eet.h>
+#include <Ecore_File.h>
+#include <unistd.h>
 
-#define EXPORT_PNG 0
-#define EXPORT_EET 1
+typedef enum
+{
+   PNG,
+   EET
+} Export_Type;
 
-/* === Setup here === */
-#define EXPORT EXPORT_EET
-
-
-#if !HAVE_EET && EXPORT == EXPORT_EET
-# warning Cannot export as EET. Defaults to PNG.
-# undef EXPORT
-# define EXPORT EXPORT_PNG
-#endif
-
-#if EXPORT == EXPORT_EET
-# include <Eet.h>
 static Eet_File *_ef = NULL;
-# define OPEN_EET(era) _ef = eet_open("../data/tiles/" era ".eet", EET_FILE_MODE_WRITE)
-# define CLOSE_EET() eet_close(_ef)
-#else
-# define OPEN_EET(era)
-# define CLOSE_EET()
-#endif
+static Export_Type _export_type;
+
+#define OPEN_EET(era) \
+   do { \
+      if (_export_type == EET) { \
+         char my_getcwd__[1024]; \
+         char my_path__[1024]; \
+         char my_path2__[1024]; \
+         snprintf(my_path2__, sizeof(my_path__), \
+                  "%s/tiles/eet", getcwd(my_getcwd__, sizeof(my_getcwd__))); \
+         ecore_file_mkpath(my_path2__); \
+         snprintf(my_path__, sizeof(my_path__), "%s/%s.eet", \
+                  my_path2__, era); \
+         _ef = eet_open(my_path__, EET_FILE_MODE_WRITE); \
+      } \
+   } while (0)
+
+#define CLOSE_EET() \
+   do { \
+      if (_export_type == EET) { \
+         eet_close(_ef); \
+      } \
+   } while (0)
 
 static void
 _usage(void)
 {
-   fprintf(stderr, "*** Usage: extract <maindat.war> [dbg lvl = 0]\n");
+   fprintf(stderr, "*** Usage: extract <maindat.war> <{eet,png}> [dbg lvl = 0]\n");
 }
-
-#if EXPORT == EXPORT_PNG
 
 static const char *
 _era2str(Pud_Era era)
@@ -53,31 +62,30 @@ _era2str(Pud_Era era)
 }
 
 static void
-_export_tile(const Pud_Color    *tile,
-             int                 w,
-             int                 h,
-             const War2_Tileset_Descriptor *ts,
-             int                 img_nb)
+_export_tile_png(const Pud_Color    *tile,
+                 int                 w,
+                 int                 h,
+                 const War2_Tileset_Descriptor *ts,
+                 int                 img_nb)
 {
-   char buf[1024];
+   char buf[1024], buf2[1024], buf3[1024];
 
    /* Fog of war */
    if (img_nb <= 16) return;
 
-   snprintf(buf, sizeof(buf), "../data/tiles/png/%s/0x%04x.png",
-            _era2str(ts->era), img_nb);
-
-   war2_png_write(buf, w, h, (unsigned char *)tile);
+   snprintf(buf, sizeof(buf),
+            "%s/tiles/png/%s", getcwd(buf3, sizeof(buf3)), _era2str(ts->era));
+   ecore_file_mkpath(buf);
+   snprintf(buf2, sizeof(buf2), "%s/0x%04x.png", buf, img_nb);
+   war2_png_write(buf2, w, h, (unsigned char *)tile);
 }
 
-#elif EXPORT == EXPORT_EET
-
 static void
-_export_tile(const Pud_Color    *tile,
-             int                 w,
-             int                 h,
-             const War2_Tileset_Descriptor *ts EINA_UNUSED,
-             int                 img_nb)
+_export_tile_eet(const Pud_Color    *tile,
+                 int                 w,
+                 int                 h,
+                 const War2_Tileset_Descriptor *ts EINA_UNUSED,
+                 int                 img_nb)
 {
    char key[8];
    unsigned char *data;
@@ -110,7 +118,6 @@ _export_tile(const Pud_Color    *tile,
 
    free(data);
 }
-#endif
 
 int
 main(int    argc,
@@ -119,12 +126,25 @@ main(int    argc,
    War2_Data *w2;
    War2_Tileset_Descriptor *ts;
    const char *file;
+   const char *type;
    int verbose;
+   char buf[1024];
 
-   if (argc >= 2)
+   if (argc >= 3)
      {
         file = argv[1];
-        verbose = (argc == 3) ? strtol(argv[2], NULL, 10) : 0;
+        type = argv[2];
+        if (!strcmp(type, "png"))
+          _export_type = PNG;
+        else if (!strcmp(type, "eet"))
+          _export_type = EET;
+        else
+          {
+             fprintf(stderr, "*** Invalid type \"%s\"\n", type);
+             _usage();
+             return 1;
+          }
+        verbose = (argc == 4) ? atoi(argv[3]) : 0;
      }
    else
      {
@@ -132,48 +152,48 @@ main(int    argc,
         return 1;
      }
 
-#if EXPORT == EXPORT_EET
    eet_init();
-#endif
-
+   ecore_file_init();
    war2_init();
+
    w2 = war2_open(file, verbose);
    if (!w2) return 1;
 
+#define EXPORT_FUNCTION() \
+   (_export_type == EET) ? _export_tile_eet : _export_tile_png
+
    OPEN_EET("forest");
-   ts = war2_tileset_decode(w2, PUD_ERA_FOREST, _export_tile);
+   ts = war2_tileset_decode(w2, PUD_ERA_FOREST, EXPORT_FUNCTION());
    if (!ts) DIE_RETURN(2, "Failed to decode tileset FOREST");
    war2_tileset_descriptor_free(ts);
    CLOSE_EET();
 
    OPEN_EET("winter");
-   ts = war2_tileset_decode(w2, PUD_ERA_WINTER, _export_tile);
+   ts = war2_tileset_decode(w2, PUD_ERA_WINTER, EXPORT_FUNCTION());
    if (!ts) DIE_RETURN(2, "Failed to decode tileset WINTER");
    war2_tileset_descriptor_free(ts);
    CLOSE_EET();
 
    OPEN_EET("wasteland");
-   ts = war2_tileset_decode(w2, PUD_ERA_WASTELAND, _export_tile);
+   ts = war2_tileset_decode(w2, PUD_ERA_WASTELAND, EXPORT_FUNCTION());
    if (!ts) DIE_RETURN(2, "Failed to decode tileset WASTELAND");
    war2_tileset_descriptor_free(ts);
    CLOSE_EET();
 
    OPEN_EET("swamp");
-   ts = war2_tileset_decode(w2, PUD_ERA_SWAMP, _export_tile);
+   ts = war2_tileset_decode(w2, PUD_ERA_SWAMP, EXPORT_FUNCTION());
    if (!ts) DIE_RETURN(2, "Failed to decode tileset SWAMP");
    war2_tileset_descriptor_free(ts);
    CLOSE_EET();
 
    war2_close(w2);
+
    war2_shutdown();
-
-#if EXPORT == EXPORT_EET
+   ecore_file_shutdown();
    eet_shutdown();
-#endif
 
-   printf("\n"
-          "Do not forget to run cmake to copy generated files\n"
-          "\n");
+   printf("Output is in %s/tiles/%s\n", getcwd(buf, sizeof(buf)),
+          (_export_type == EET) ? "eet" : "png");
 
    return 0;
 }

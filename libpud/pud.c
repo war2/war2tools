dd6cd2f5cb8985884ae04d8f69725c54ab6d30a3
Jean Guyomarc'h <jean@guyomarch.bzh>
2016-09-18 15:54:59 +0200

    libpud: use immediate macro to write defaults upgrade

--- libpud/pud.c
+++ libpud/pud.c
@@ -514,7 +514,7 @@ pud_write(const Pud  *pud,
 
    /* Section UGRD */
    WISEC(PUD_SECTION_UGRD, 782);
-   w = p->default_ugrd; W16(w, 1);
+   WI16(p->default_ugrd, 1);
    for (i = 0; i < 52; i++) W8(p->upgrade[i].time, 1);
    for (i = 0; i < 52; i++) W16(p->upgrade[i].gold, 1);
    for (i = 0; i < 52; i++) W16(p->upgrade[i].lumber, 1);

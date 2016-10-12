947440ee4d8222295f041faf5c9712506242f13d
Jean Guyomarc'h <jean@guyomarch.bzh>
2016-10-12 10:55:15 +0200

    libpud: better check for mmap() presence
    Let CMake do what it is paid to do...

--- libpud/mmap.c
+++ libpud/mmap.c
@@ -2,35 +2,23 @@
  * mmap.c
  * libpud
  *
- * Copyright (c) 2014 Jean Guyomarc'h
+ * Copyright (c) 2014 - 2016 Jean Guyomarc'h
  */
 
 #include "pud_private.h"
 
-#if defined(__linux__) || (defined(__APPLE__) && defined(__MACH__))
-# define POSIX_COMPLIANT 1
-#else
-# define POSIX_COMPLIANT 0
-#endif
-
-#if POSIX_COMPLIANT
+#ifdef HAVE_MMAP
 # include <fcntl.h>
 # include <sys/stat.h>
 # include <sys/mman.h>
 # include <unistd.h>
 #endif
 
-#if POSIX_COMPLIANT /* ... */
-# define PUD_MMAP_ENABLED 1
-#else
-# define PUD_MMAP_ENABLED 0
-#endif
-
 void *
 pud_mmap(const char *file,
          size_t     *size_ret)
 {
-#if PUD_MMAP_ENABLED
+#ifdef HAVE_MMAP
 
    void *map = NULL;
    struct stat s;
@@ -66,9 +54,9 @@ err_close:
 
 void
 pud_munmap(void   *map,
-          size_t  size)
+           size_t  size)
 {
-#if PUD_MMAP_ENABLED
+#ifdef HAVE_MMAP
    munmap(map, size);
 #else
    fprintf(stderr,

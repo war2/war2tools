de9c6732497cae75ee581de5410efd4256875497
Jean Guyomarc'h <jean@guyomarch.bzh>
2016-10-15 19:58:43 +0200

    libpud: add license in all source files

--- libpud/private.c
+++ libpud/private.c
@@ -1,13 +1,28 @@
 /*
- * private.c
- * libpud
+ * Copyright (c) 2014-2016 Jean Guyomarc'h
  *
- * Copyright (c) 2014 Jean Guyomarc'h
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
  */
 
 #include "pud_private.h"
 
-const char *
+PUDAPI_INTERNAL const char *
 long2bin(uint32_t x)
 {
    static char buf[64];
@@ -46,7 +61,7 @@ long2bin(uint32_t x)
    return buf;
 }
 
-const char *
+PUDAPI_INTERNAL const char *
 mode2str(Pud_Open_Mode mode)
 {
    if (mode & PUD_OPEN_MODE_R)
@@ -63,7 +78,7 @@ mode2str(Pud_Open_Mode mode)
    return "rb";
 }
 
-Pud_Color
+PUDAPI_INTERNAL Pud_Color
 color_make(uint8_t r,
            uint8_t g,
            uint8_t b,
@@ -78,7 +93,7 @@ color_make(uint8_t r,
    return c;
 }
 
-const char *
+PUDAPI_INTERNAL const char *
 dim2str(Pud_Dimensions dim)
 {
    switch (dim)

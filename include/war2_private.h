fe681c38828e82a0895b55e31e500946284a1e10
Jean Guyomarc'h <jean@guyomarch.bzh>
2016-10-16 08:34:23 +0200

    libwar2: add license in source files, handle private API

--- include/war2_private.h
+++ include/war2_private.h
@@ -1,8 +1,23 @@
 /*
- * war2_private.h
- * libwar2
+ * Copyright (c) 2014-2016 Jean Guyomarc'h
  *
- * Copyright (c) 2014 Jean Guyomarc'h
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
  */
 
 #ifndef _WAR2_PRIVATE_H_
@@ -99,8 +114,12 @@ struct _War2_Data
       } \
    } while (0)
 
+static inline Pud_Bool
+war2_mem_map_ok(War2_Data *w2)
+{
+   return (w2->ptr < w2->mem_map + w2->mem_map_size);
+}
 
-Pud_Bool war2_mem_map_ok(War2_Data *w2);
-void war2_palette_convert(unsigned char *ptr, Pud_Color palette[256]);
+PUDAPI_INTERNAL void war2_palette_convert(const unsigned char *ptr, Pud_Color palette[256]);
 
 #endif /* ! _WAR2_PRIVATE_H_ */
